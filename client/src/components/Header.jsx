53cbc09a1fa89e4cf8e58e1468a8ca93d2227cd0
masorol <miasorola.yoshida@gmail.com>
2024-10-13 10:45:59 -0500

    feat: favicon added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,18 @@
 import logo from "../files/ctrlflogo.png";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/");
+  };
+
   return (
     <header className="bg-[#3c799a] text-white p-4">
-      <div className="container mx-auto flex items-center">
+      <div
+        onClick={handleClick}
+        className="container mx-auto flex items-center cursor-pointer"
+      >
         <img src={logo} alt="Logo" className="h-10 mr-2" />{" "}
         <h1 className="text-2xl font-bold">Better Ctrl+F</h1>
       </div>
