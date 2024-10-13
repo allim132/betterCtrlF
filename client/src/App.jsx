bab19153da31c1f3244ea55b1de7a0b9d198e41b
Alex Nguyen <amn7417@mavs.uta.edu>
2024-10-12 21:20:57 -0500

    <fix>: This commit will correctly allow tailwindcss to run.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
+import React from "react"
+
 function App() {
   return (
     <>
-      <div className="flex items-center justify-center h-screen">HELLO</div>
+      <div className="flex items-center justify-center h-screen">
+        <p>HELLO ASDAS</p>
+      </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,6 @@ export default {
     "./index.html",
     "./src/**/*{js,ts,jsx,tsx}",
     "./src/*{js,ts,jsx,tsx}",
-    "./src/App.jsx",
   ],
   theme: {
     extend: {},
